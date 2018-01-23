2d0f3bc004854eeeb99ee0af5f50e85e04fde6e5
richierich110 <35575264+richierich110@users.noreply.github.com>
2018-01-23 12:40:18 -0500

    Weapon Changes
    Fixed Weapon base class and added 4 different weapon derived classes.

---
+++ Assets/Scripts/Sniper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sniper : Weapon {
+
+	// Use this for initialization
+	void Start () {
+        Range = 100;
+        Damage = 40;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
