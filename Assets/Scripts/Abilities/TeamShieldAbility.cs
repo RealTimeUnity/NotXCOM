082e6423d280a3944e7331f29e17f1c045f66fa5
evanFroelich <31809194+evanFroelich@users.noreply.github.com>
2018-02-11 19:32:30 -0500

    ahhhhh

---
+++ Assets/Scripts/Abilities/TeamShieldAbility.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamShieldAbility : Ability
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < owner.owner.friendlies.Count; i++)
+        {
+            float distance = Mathf.Sqrt(Mathf.Pow((owner.owner.friendlies[i].gameObject.transform.position.x - this.owner.gameObject.transform.position.x), 2) +
+                    Mathf.Pow((owner.owner.friendlies[i].gameObject.transform.position.z - this.owner.gameObject.transform.position.z), 2));
+            if (distance < 30)
+            {
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
